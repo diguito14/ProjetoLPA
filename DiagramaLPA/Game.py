f6519512f9371e9b0ad2b937660a98091086617d
Diego <diguito14@gmail.com>
2023-08-14 19:07:32 -0300

    Signed-off-by: Diego <diguito14@gmail.com>

---
+++ DiagramaLPA/Game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Game:
+    def __init__(self):
+        self.window = None
+        self.Attribute1 = None
+        self.Attribute2 = None
+
+    def run(self, ):
+        pass
+
